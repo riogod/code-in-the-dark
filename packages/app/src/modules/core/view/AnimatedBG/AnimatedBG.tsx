7e56e035ca44f3194bdfe73f07c87794866abbb4
riogod <riogod@gmail.com>
2023-12-01 18:02:10 +0300

    fix:  level0 image

--- packages/app/src/modules/core/view/AnimatedBG/AnimatedBG.tsx
+++ packages/app/src/modules/core/view/AnimatedBG/AnimatedBG.tsx
@@ -1,7 +1,8 @@
 import { FC, ReactNode } from "react";
 import { observer } from "mobx-react-lite";
 import Box from "@mui/material/Box";
-import NotFoundImage from "../../assets/20282271_6205013.svg";
+import StartScreenBG from "../../assets/20282271_6205013.svg";
+import Level0BG from "../../assets/level0bg.png";
 import {useRoute} from "react-router5";
 import {useVM} from "../../../../ui/hooks/useVM.ts";
 import {GameViewModel} from "../../../code_in_the_dark/viewmodels/game.vm.ts";
@@ -15,21 +16,24 @@ const AnimatedBG: FC<{ children: ReactNode }> = ({ children }) => {
     return <Box sx={{
       height: "100vh",
       animation: 'AnimateBG 20s ease infinite',
-      backgroundImage: 'url(' + NotFoundImage + ')'
+      backgroundImage: 'url(' + StartScreenBG + ')'
     }}>{children}</Box>;
   }
 
   const bg = () => {
 
-
+// всего 5 уровней через каждые 50 очков
   if(gameVM.level === 0){
 
     return <Box sx={{
       height: "100vh",
       width: "100vw",
       position: 'fixed',
-      animation: 'AnimateBG 20s ease infinite',
-      backgroundImage: 'url(' + NotFoundImage + ')'
+      backgroundColor: 'black',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      opacity: 0.3,
+      backgroundImage: 'url(' + Level0BG + ')'
     }}/>;
   }
   }
