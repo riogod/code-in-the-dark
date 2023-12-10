6d5fe0c3f83df2aa5fed892bdfee27123c0a90db
riogod <riogod@gmail.com>
2023-12-10 16:00:03 +0300

    fix: some bg and theme fixes

--- packages/app/src/modules/code_in_the_dark/view/components/glitchStyles.ts
+++ packages/app/src/modules/code_in_the_dark/view/components/glitchStyles.ts
@@ -1,57 +1,57 @@
-import { Theme } from '@mui/material';
-import { makeStyles } from '@mui/styles'; 
+import { createStyles, makeStyles } from "@mui/styles";
 
 function random(max: number, min = 0): number {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 const mixinForPsevdo = {
-    clip: 'rect(44px, 450px, 56px, 0)',
-    content: 'attr(data-text)',
-    height: '100%',
-    width: '100%',
-    position: 'absolute',
-    top: '0',
-}
+  clip: "rect(44px, 450px, 56px, 0)",
+  content: "attr(data-text)",
+  height: "100%",
+  width: "100%",
+  position: "absolute",
+  top: "0",
+};
 const keyframes = () => {
-    const result: { [key: string]: {[key: string]: string;}; } = {};
+  const result: { [key: string]: { [key: string]: string } } = {};
 
-    for (let i = 0; i <= 100; i+=5) {
-        result[`${i}%`] = {
-            clip: `rect(${random(100)}px, 9999px, ${random(100)}px, 0)`,
-            transform: `skew((${random(100) / 100})deg)`,
-        }
-    }
-    
-    return result
-}
-
-const useGlitchStyles = makeStyles((theme: Theme) => ({
-  title: {
-    color: theme.palette.text.primary,
-    position: 'relative',
-    display: 'block',
-    textAlign: 'center',
-    '&::before': {
-      ...mixinForPsevdo,
-      animation: '$glitch 6s infinite linear alternate-reverse',
-      textShadow: '-2px 0 #ff00c1',
-      left: '2px'
-    },
-    '&::after': {
-      ...mixinForPsevdo,
-      animation: '$glitchTwo 3s infinite linear alternate-reverse',
-      textShadow: '-2px 0 #00fff9, 2px 2px #ff00c1',
-      left: '-2px'
-    },
-  },
-  '@keyframes glitch': {
-    ...keyframes()
-  },
-  '@keyframes glitchTwo': {
-    ...keyframes()
+  for (let i = 0; i <= 100; i += 5) {
+    result[`${i}%`] = {
+      clip: `rect(${random(100)}px, 9999px, ${random(100)}px, 0)`,
+      transform: `skew((${random(100) / 100})deg)`,
+    };
   }
-}));
 
+  return result;
+};
 
-export {useGlitchStyles};
+const useGlitchStyles = makeStyles(() =>
+  createStyles({
+    title: {
+      color: "white",
+      position: "relative",
+      display: "block",
+      textAlign: "center",
+      "&::before": {
+        ...mixinForPsevdo,
+        animation: "$glitch 6s infinite linear alternate-reverse",
+        textShadow: "-2px 0 #ff00c1",
+        left: "2px",
+      },
+      "&::after": {
+        ...mixinForPsevdo,
+        animation: "$glitchTwo 3s infinite linear alternate-reverse",
+        textShadow: "-2px 0 #00fff9, 2px 2px #ff00c1",
+        left: "-2px",
+      },
+    },
+    "@keyframes glitch": {
+      ...keyframes(),
+    },
+    "@keyframes glitchTwo": {
+      ...keyframes(),
+    },
+  }),
+);
+
+export { useGlitchStyles };
